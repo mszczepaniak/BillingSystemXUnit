6a498308cbf27512244533288f7ee283bafda2fe
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-02-04 08:49:06 +0100

    WIP writing first test in xunit, tdd approach

---
+++ BillingSystem.Tests/BillingDoohickeyTests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using Xunit;
+
+namespace BillingSystem.Tests
+{
+    public class BillingDoohickeyTests
+    {
+        [Fact]
+        public void CustomerWhoDoesNotHaveSubscriptionDoesNotGetCharged()
+        {
+            // Source of customers
+            // Service for charging customers
+            var repo = new Mock<ICustomerRepository>();
+            var charger = new Mock<ICreditCardCharger>();
+            var customer = new Customer();
+
+            BillingDoohickey thing = new BillingDoohickey(repo, charger);
+
+            thing.ProcessMonth(2011, 8);
+
+            charger.Verify(c => c.ChargeCustomer(customer), Times.Never);
+            //
+        }
+        // Monthly billing
+        // Grace period for missed payments ("dunning" status)
+        // Not all customers are necessarily subscribers
+        // Idle customers should be automatically unsubscribed
+    }
+
+    public interface ICustomerRepository
+    {
+        
+    }
+
+    public interface ICreditCardCharger
+    {
+        
+    }
+
+    public class Customer
+    {
+        
+    }
+
+    public class BillingDoohickey
+    {
+        ICreditCardCharger charger;
+        ICustomerRepository repo;
+
+        public BillingDoohickey(ICustomerRepository repo, ICreditCardCharger charger)
+        {
+
+            this.repo = repo;
+            this.charger = charger;
+        }
+    }
+}
